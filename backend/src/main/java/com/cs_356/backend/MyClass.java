e23b4be2dd8c9d2a5ff072b2c537084f0521ce43
cwood <cwood@efilecabinet.com>
2022-02-28 08:15:38 -0700

    Changed Java Language Version

--- backend/src/main/java/com/cs_356/backend/MyClass.java
+++ backend/src/main/java/com/cs_356/backend/MyClass.java
@@ -1,4 +1,9 @@
 package com.cs_356.backend;
 
-public class MyClass {
+public class MyClass
+{
+    public static void main( String... args ) throws Exception
+    {
+        System.out.println("Hello World");
+    }
 }
