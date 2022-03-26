be8ff012ed874c934048c3cc64aaa47034fe91db
Chayston Wood <chaystonwood@gmail.com>
2022-03-25 18:31:45 -0600

    Fix merge conflict

--- app/src/main/java/com/cs_356/app/Views/BarcodeScannerFragment.java
+++ app/src/main/java/com/cs_356/app/Views/BarcodeScannerFragment.java
@@ -1,5 +1,6 @@
 package com.cs_356.app.Views;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,8 @@ import android.view.ViewGroup;
 
 import com.cs_356.app.R;
 import com.cs_356.app.databinding.FragmentBarcodeScannerBinding;
+
+import java.util.Objects;
 
 public class BarcodeScannerFragment extends Fragment {
 
@@ -36,6 +39,16 @@ public class BarcodeScannerFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(BarcodeScannerFragment.this)
                         .navigate(R.id.action_BarcodeScannerFragment_to_GameScannedFragment);
+            }
+        });
+
+        binding.scanBarcodeBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requireActivity().startActivity(
+                        new Intent(getActivity(), GameLibraryActivity.class)
+                );
+                requireActivity().finish();
             }
         });
     }
