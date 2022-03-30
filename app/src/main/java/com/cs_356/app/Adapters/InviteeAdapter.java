cc292efbe3975db612236f018aaa1470cb6c8597
brayd <braydenwood1@gmail.com>
2022-03-30 10:13:34 -0600

    added recycler view for invitee list

---
+++ app/src/main/java/com/cs_356/app/Adapters/InviteeAdapter.java
@@ -0,0 +1,69 @@
+package com.cs_356.app.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cs_356.app.R;
+import com.cs_356.app.Utils.Image.PicassoTransformations;
+import com.cs_356.app.Views.AddGameActivity;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import Entities.BoardGame;
+import Entities.User;
+import jp.wasabeef.picasso.transformations.BlurTransformation;
+import jp.wasabeef.picasso.transformations.CropSquareTransformation;
+
+public class InviteeAdapter extends RecyclerView.Adapter<InviteeAdapter.ViewHolder> {
+
+    private final List<User> inviteeList;
+
+    public InviteeAdapter(List<User> inviteeList) {
+        this.inviteeList = inviteeList;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView name;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.inviteeName);
+        }
+
+        public TextView getName() {
+            return name;
+        }
+    }
+
+    @Override
+    public InviteeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View gameView = inflater.inflate(R.layout.invitee_item, parent, false);
+        return new ViewHolder(gameView);
+    }
+
+    @Override
+    public void onBindViewHolder(InviteeAdapter.ViewHolder holder, int position) {
+        User invitee = inviteeList.get(position);
+        holder.getName().setText(invitee.getFirstName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return inviteeList.size();
+    }
+}
--- app/src/main/java/com/cs_356/app/Views/AddGameNight/AddGameNightFriendsFragment.java
+++ app/src/main/java/com/cs_356/app/Views/AddGameNight/AddGameNightFriendsFragment.java
@@ -3,59 +3,34 @@ package com.cs_356.app.Views.AddGameNight;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.cs_356.app.Adapters.GameCardAdapter;
+import com.cs_356.app.Adapters.InviteeAdapter;
 import com.cs_356.app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddGameNightFriendsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import Entities.User;
+
 public class AddGameNightFriendsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public RecyclerView recyclerView;
+    public Button addGameNightFinishButton;
 
     public AddGameNightFriendsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddGameNight2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddGameNightFriendsFragment newInstance(String param1, String param2) {
-        AddGameNightFriendsFragment fragment = new AddGameNightFriendsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,12 +38,54 @@ public class AddGameNightFriendsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_add_game_night_friends, container, false);
-        final Button addGameNightFinishButton = rootView.findViewById(R.id.add_game_night_finish_button);
+
+        // Set on click for finish button
+        addGameNightFinishButton = rootView.findViewById(R.id.add_game_night_finish_button);
         addGameNightFinishButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ((AddGameNightActivity)getActivity()).onClickFinishButton();
             }
         });
+
+        recyclerView = rootView.findViewById(R.id.inviteeRecyclerView);
+        // Set up adapter for recycler view
+        User user1 = new User("555", "Brayden", "Wood", "pass");
+        User user2 = new User("555", "Chayston", "Wood", "pass");
+        User user3 = new User("555", "Jake", "Wood", "pass");
+        User user4 = new User("555", "Daniel", "Wood", "pass");
+        User user5 = new User("555", "Brayden", "Wood", "pass");
+        User user6 = new User("555", "Chayston", "Wood", "pass");
+        User user7 = new User("555", "Jake", "Wood", "pass");
+        User user8 = new User("555", "Daniel", "Wood", "pass");
+        User user9 = new User("555", "Brayden", "Wood", "pass");
+        User user10 = new User("555", "Chayston", "Wood", "pass");
+        User user11 = new User("555", "Jake", "Wood", "pass");
+        User user12 = new User("555", "Daniel", "Wood", "pass");
+        User user13 = new User("555", "Brayden", "Wood", "pass");
+        User user14 = new User("555", "Chayston", "Wood", "pass");
+        User user15 = new User("555", "Jake", "Wood", "pass");
+        User user16 = new User("555", "Daniel", "Wood", "pass");
+        List<User> users = new ArrayList<User>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
+        users.add(user6);
+        users.add(user7);
+        users.add(user8);
+        users.add(user9);
+        users.add(user10);
+        users.add(user11);
+        users.add(user12);
+        users.add(user13);
+        users.add(user14);
+        users.add(user15);
+        users.add(user16);
+        InviteeAdapter adapter = new InviteeAdapter(users);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setVisibility(View.VISIBLE);
+
         return rootView;
     }
 }
