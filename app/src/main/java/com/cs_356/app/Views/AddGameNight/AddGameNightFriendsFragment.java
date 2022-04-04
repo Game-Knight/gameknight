d44b50453d20a1b1faad156d2137e84bceeabd5e
brayd <braydenwood1@gmail.com>
2022-04-03 20:45:36 -0600

    made invitee list prettier

--- app/src/main/java/com/cs_356/app/Views/AddGameNight/AddGameNightFriendsFragment.java
+++ app/src/main/java/com/cs_356/app/Views/AddGameNight/AddGameNightFriendsFragment.java
@@ -3,6 +3,7 @@ package com.cs_356.app.Views.AddGameNight;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -54,6 +55,10 @@ public class AddGameNightFriendsFragment extends Fragment {
         InviteeAdapter adapter = new InviteeAdapter(invitees);
         recyclerView.setAdapter(adapter);
         recyclerView.setVisibility(View.VISIBLE);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
 
         return rootView;
     }
