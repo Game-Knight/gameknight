1db24debd772524131096832064adcbe417acdad
Daniel Taylor <dptaylor98@gmail.com>
2022-04-13 01:46:11 -0600

    add padding, change duration input type, fix time logic

--- app/src/main/java/com/cs_356/app/Views/AddGameNight/TimePickerFragment.java
+++ app/src/main/java/com/cs_356/app/Views/AddGameNight/TimePickerFragment.java
@@ -21,10 +21,13 @@ public class TimePickerFragment extends DialogFragment
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+//        // Use the current time as the default values for the picker
+//        final Calendar c = Calendar.getInstance();
+//        int hour = c.get(Calendar.HOUR_OF_DAY);
+//        int minute = c.get(Calendar.MINUTE);
+        // Use 12 pm as the default value for the picker
+        int hour = 12;
+        int minute = 0;
 
 
         // Create a new instance of TimePickerDialog and return it
@@ -34,17 +37,20 @@ public class TimePickerFragment extends DialogFragment
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         String modifier;
-        if (hourOfDay > 12) {
-            hourOfDay = hourOfDay % 12;
-            modifier = "pm";
+        if (hourOfDay >= 12) {
+            if (hourOfDay != 12) {
+                hourOfDay = hourOfDay % 12;
+            }
+            modifier = " pm";
         }
         else {
             if (hourOfDay == 0) {
                 hourOfDay = 12;
             }
-            modifier = "am";
+            modifier = " am";
         }
-        String time = Integer.toString(hourOfDay) + ":" + Integer.toString(minute) + modifier;
+        String minuteString = (minute ==  0 ? '0' + Integer.toString(minute) : Integer.toString(minute));
+        String time = Integer.toString(hourOfDay) + ":" + minuteString + modifier;
         ((AddGameNightActivity)getActivity()).setSelectTimeTextView(time);
     }
 }
